b8f0072f36f45f21039362f010d62f6caa3a3141
cons6tance <mangle11171@gmail.com>
2024-06-25 13:22:57 -0300

    creo una rama aparte del proyecto, cambios en la barra de navegación y comienzo a desarrollar banner

--- miproyecto_django/miapp_CODEMC/static/miapp_CODEMC/js/main.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".menu-toggle").addEventListener("click", function() {
-  this.classList.toggle("active");
-});
-
-
-document.querySelector(".menu-toggle").addEventListener("click", function() {
-    document.getElementById("sidebar").classList.toggle("active");
-});
-  
--- miproyecto_django/miapp_CODEMC/views.py
+++ miproyecto_django/miapp_CODEMC/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 from django.db import connection
 # Create your views here.
 def index(request):
-    contexto_random = {}
-    return render(request,'miapp_CODEMC/index.html',contexto_random)
+    ctx = {}
+    return render(request,'miapp_CODEMC/index.html',ctx)
