57c2eff188d17838c3a3f1ffc3ab72d5374c5895
BombasticDr <dr5007661@gmail.com>
2024-09-24 18:15:25 -0300

    Update views.py

--- miproyecto_django/miapp_CODEMC/views.py
+++ miproyecto_django/miapp_CODEMC/views.py
@@ -1,22 +1,29 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.hashers import make_password
 from django.http import HttpResponse
 from django.db import connection
-import bcrypt
-
+from django.contrib import messages
 
 from . import forms
+
 # Create your views here.
 def index(request):
     ctx = {}
-    return render(request,'miapp_CODEMC/index.html',ctx)
-
+    return render(request, 'miapp_CODEMC/index.html', ctx)
 
 def planes(request):
-    return render(request,"miapp_CODEMC/Planes.html")
+    return render(request, "miapp_CODEMC/Planes.html")
 
 def contacto(request):
     return render(request, "miapp_CODEMC/Contacto.html")
+
+def inicio_gestion(request):
+    return render(request, "miapp_codemc/inicio_gestion.html")
+
+def sucursales(request):
+    return render(request, "miapp_codemc/sucursales.html")
+
+def estadisticas(request):
+    return render(request, "miapp_codemc/estadisticas.html")
 
 def inicio_usuario(request):
     if request.method == 'POST':
@@ -24,28 +31,27 @@ def inicio_usuario(request):
         if form.is_valid():
             email = form.cleaned_data["gmail"]
             contraseña = form.cleaned_data["contraseña"]
-            #Verifica si el usuario existe y la contraseña es correcta
+            # Verifica si el usuario existe y la contraseña es correcta
 
             with connection.cursor() as cursor:
-                # Obtén el hash de la contraseña almacenado
+                # Obtén la contraseña almacenada en texto plano
                 cursor.execute("SELECT contraseña FROM usuarios WHERE email = %s", [email])
-                cursor= cursor.fetchone()
+                cursor = cursor.fetchone()
 
                 if cursor:
-                    stored_hash = cursor[0]
+                    stored_password = cursor[0]
 
-                    # Verifica la contraseña
-                    if bcrypt.checkpw(contraseña.encode('utf-8'), stored_hash.encode('utf-8')):
-                        return HttpResponse("Inicio de sesión exitoso")
+                    # Verifica la contraseña en texto plano
+                    if contraseña == stored_password:
+                        return redirect("inicio gestion")
                     else:
-                        return HttpResponse("Contraseña incorrecta")
+                        messages.error(request, "Contraseña incorrecta. Inténtalo de nuevo.")
                 else:
-                    return HttpResponse("Usuario no encontrado")  
+                    messages.error(request, "Usuario no encontrado.")
     else:
         form = forms.FormularioLogin()
-    ctx = {"form":form}
-    return render(request, "miapp_CODEMC/Inicio_login.html",ctx)
-
+    ctx = {"form": form}
+    return render(request, "miapp_CODEMC/Inicio_login.html", ctx)
 
 def inicio_registro(request):
     if request.method == 'POST':
@@ -65,14 +71,13 @@ def inicio_registro(request):
             if email_count > 0:
                 return HttpResponse("Este correo electrónico ya está registrado.")
 
-            # Insertar el nuevo usuario en la base de datos
-            hashed = bcrypt.hashpw(contraseña.encode('utf-8'), bcrypt.gensalt())
+            # Insertar el nuevo usuario en la base de datos con la contraseña en texto plano
             with connection.cursor() as cursor:
-                cursor.execute("INSERT INTO usuarios (email, contraseña) VALUES (%s, %s)", [email, hashed])
+                cursor.execute("INSERT INTO usuarios (email, contraseña) VALUES (%s, %s)", [email, contraseña])
 
             return redirect('login')  # Redirige a la página de inicio de sesión o a otra página
     else:
         form = forms.FormularioRegistro()
-    ctx = {"form":form}
+    ctx = {"form": form}
     return render(request, "miapp_CODEMC/inicio_registro.html", ctx)
 
