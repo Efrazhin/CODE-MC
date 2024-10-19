e7579be0124cebec151d3aeaca94acac9ae3d327
BombasticDr <dr5007661@gmail.com>
2024-10-19 16:56:53 -0300

    Update forms.py
    defini de forma basica todos los formularios que llegaremos a usar

--- miproyecto_django/miapp_CODEMC/forms.py
+++ miproyecto_django/miapp_CODEMC/forms.py
@@ -17,7 +17,7 @@ class FormRegistroUser(UserCreationForm):
 class FormCliente(ModelForm):
     class Meta:
         model = Cliente
-        fields = []
+        fields = ["dni_cliente","nombre","apellido","calle","nro_calle","telefono","email","fecha_nacimiento","empresa"]
 
 class CategoriaForm(ModelForm):
     class Meta:
@@ -27,4 +27,157 @@ class CategoriaForm(ModelForm):
             'nombre': 'Nombre',
             'descripcion': 'Descripción',
             'empresa': 'Empresa',
+        }
+
+class ProvinciaForm(ModelForm):
+    class Meta:
+        model = Provincia
+        fields=["nombre"]
+
+
+class PaisForm(ModelForm):
+    class Meta:
+        model = Pais
+        fields=["nombre"]
+
+class ProveedorForm(ModelForm):
+    class Meta:
+        model = Proveedor
+        fields = [
+            'dni_proveedor', 'nombre', 'apellido', 'cuit', 'telefono', 
+            'email', 'calle', 'ciudad', 'provincia', 'pais', 
+            'descripcion', 'web', 'comentarios', 'nro_calle', 'empresa'
+        ]
+
+class AlmacenForm(ModelForm):
+    class Meta:
+        model = Almacen
+        fields = [
+            'telefono', 'provincia', 'ciudad', 
+            'calle', 'nro_calle', 'tamaño', 'unidad_medida', 'empresa'
+        ]
+        labels = {
+            'telefono': 'Teléfono',
+            'provincia': 'Provincia',
+            'ciudad': 'Ciudad',
+            'calle': 'Calle',
+            'nro_calle': 'Número de Calle',
+            'tamaño': 'Tamaño',
+            'unidad_medida': 'Unidad de Medida',
+            'empresa': 'Empresa',
+        }
+class SucursalForm(ModelForm):
+    class Meta:
+        model = Sucursal
+        fields = ['telefono', 'provincia', 'ciudad', 'calle', 'nro_calle', 'almacen', 'empresa']
+
+class SubcategoriaForm(ModelForm):
+    class Meta:
+        model = Subcategoria
+        fields = ['nombre', 'descripcion', 'categoria']
+        labels = {
+            'nombre': 'Nombre',
+            'descripcion': 'Descripción',
+            'categoria': 'Categoría',
+        }
+
+class StockForm(ModelForm):
+    class Meta:
+        model = Stock
+        fields = ['cantidad']
+        labels = {
+            'cantidad': 'Cantidad',
+        }
+
+class ProductoForm(ModelForm):
+    class Meta:
+        model = Producto
+        fields = [
+            'nombre', 'descripcion', 'precio', 'tamaño', 
+            'unidad_medida', 'fecha_vencimiento', 'subcategoria', 
+            'categoria', 'stock', 'almacen', 'sucursal'
+        ]
+        labels = {
+            'nombre': 'Nombre',
+            'descripcion': 'Descripción',
+            'precio': 'Precio',
+            'tamaño': 'Tamaño',
+            'unidad_medida': 'Unidad de Medida',
+            'fecha_vencimiento': 'Fecha de Vencimiento',
+            'subcategoria': 'Subcategoría',
+            'categoria': 'Categoría',
+            'stock': 'Stock',
+            'almacen': 'Almacén',
+            'sucursal': 'Sucursal',
+        }
+
+class RemitoForm(ModelForm):
+    class Meta:
+        model = Remito
+        fields = ['fecha', 'descripcion', 'cliente', 'empleado', 'almacen', 'sucursal']
+        labels = {
+            'fecha': 'Fecha',
+            'descripcion': 'Descripción',
+            'cliente': 'Cliente',
+            'empleado': 'Empleado',
+            'almacen': 'Almacén',
+            'sucursal': 'Sucursal',
+        }
+
+class CompraForm(ModelForm):
+    class Meta:
+        model = Compra
+        fields = ['fecha', 'descripcion', 'proveedor', 'empleado']
+        labels = {
+            'fecha': 'Fecha',
+            'descripcion': 'Descripción',
+            'proveedor': 'Proveedor',
+            'empleado': 'Empleado',
+        }
+
+class DetalleRemitoForm(ModelForm):
+    class Meta:
+        model = DetalleRemito
+        fields = ['producto', 'cantidad', 'descuento', 'importe', 'remito']
+        labels = {
+            'producto': 'Producto',
+            'cantidad': 'Cantidad',
+            'descuento': 'Descuento',
+            'importe': 'Importe',
+            'remito': 'Remito',
+        }
+
+class DetalleCompraForm(ModelForm):
+    class Meta:
+        model = DetalleCompra
+        fields = ['producto', 'cantidad', 'importe', 'compra']
+        labels = {
+            'producto': 'Producto',
+            'cantidad': 'Cantidad',
+            'importe': 'Importe',
+            'compra': 'Compra',
+        }
+
+class PresupuestoForm(ModelForm):
+    class Meta:
+        model = Presupuesto
+        fields = ['fecha', 'descripcion', 'cliente', 'empleado', 'sucursal']
+        labels = {
+            'fecha': 'Fecha',
+            'descripcion': 'Descripción',
+            'cliente': 'Cliente',
+            'empleado': 'Empleado',
+            'sucursal': 'Sucursal',
+        }
+
+class DetallePresupuestoForm(ModelForm):
+    class Meta:
+        model = DetallePresupuesto
+        fields = ['producto', 'cantidad', 'descuento', 'importe', 'presupuesto']
+        labels = {
+            'producto': 'Producto',
+            'cantidad': 'Cantidad',
+            'descuento': 'Descuento',
+            'importe': 'Importe',
+            'presupuesto': 'Presupuesto',
         }
